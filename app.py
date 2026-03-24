359db2a185b298753b66349cadb833ca2a4922cb
Robert <robamirbekian@gmail.com>
2026-03-23 18:56:30 -0700

    Fix app.py

--- app.py
+++ app.py
@@ -129,4 +129,6 @@ def on_disconnect():
         del rooms[room_id]
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
